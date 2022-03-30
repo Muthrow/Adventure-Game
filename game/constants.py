57acfc9994cec1a67597ac0d903028b3aed13794
Jace <kaodremora@outlook.com>
2022-03-30 16:37:10 -0400

    increase map size/ changed everything associated

--- game/constants.py
+++ game/constants.py
@@ -1,11 +1,11 @@
 from os import path
 
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 960
 SCREEN_TITLE = "Game Title"
 RESOURCE_PATH = f"{path.dirname(path.abspath(__file__))}/resources/"    #Might need to be changed
-MAP_SCALING = .55
-PLAYER_SCALE = .8
+MAP_SCALING = 1.1
+PLAYER_SCALE = 1.6
 ENEMY_SCALE = PLAYER_SCALE/2
 PLAYER_SPEED = 1
 PLAYER_UP = ["Player/exec1_up (2).png", "Player/exec1_up (1).png",
